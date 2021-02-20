6c092b4362bbd29f6ac262801009bd6fef7bccbc
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2021-02-20 12:53:57 -0300

    catalogitem

---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,32 @@
+import React, { useCallback } from 'react';
+import { addProductToCart } from '../store/modules/cart/actions';
+import { useDispatch } from 'react-redux';
+import { IProduct } from '../store/modules/cart/types';
+
+interface CatalogItemProps {
+  product: IProduct;
+}
+
+const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
+  const dispatch = useDispatch();
+  const handleAddProductToCart = useCallback(
+    () => {
+      dispatch(addProductToCart(product));
+    },
+    [dispatch, product]
+  );
+  return (
+    <article>
+      <strong>{product.title}</strong>
+      {' - '}
+      <span>{product.price}</span>
+      {'  '}
+
+      <button onClick={handleAddProductToCart} type="button">
+        Comprar
+      </button>
+    </article>
+  );
+};
+
+export default CatalogItem;
