67cf56fe1e936ceef1e85e72adfd2d8c582f926f
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2021-02-20 01:06:42 -0300

    action

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import api from '../services/api';
+import { addProductToCart } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
 
 const Catalog: React.FC = () => {
+  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -10,6 +13,13 @@ const Catalog: React.FC = () => {
       setCatalog(response.data);
     });
   }, []);
+
+  const handleAddProductToCart = useCallback(
+    (product: IProduct) => {
+      dispatch(addProductToCart(product));
+    },
+    [dispatch]
+  );
 
   return (
     <main>
@@ -22,7 +32,9 @@ const Catalog: React.FC = () => {
           <span>{product.price}</span>
           {'  '}
 
-          <button type="button">Comprar</button>
+          <button onClick={() => handleAddProductToCart(product)} type="button">
+            Comprar
+          </button>
         </article>
       ))}
     </main>
