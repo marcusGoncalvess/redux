11f222d540c95f49c8abc9d526a57c3e5fc1c87c
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2021-02-20 12:31:07 -0300

    changing state

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import Catalog from './components/Catalog';
-import './styles.css';;
+import './styles.css';import Cart from './components/Cart';
+;
 
 function App() {
   return (
     <Provider store={store}>
       <Catalog />
+      <Cart />
     </Provider>
   );
 }
